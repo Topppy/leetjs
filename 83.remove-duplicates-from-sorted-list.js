5e9564f9180499eb8553ffd8cc7af25e89a3dd2c
Topppy <taoyiming2010@126.com>
2019-08-23 20:05:44 +0800

    83

--- 83.remove-duplicates-from-sorted-list.js
+++ 83.remove-duplicates-from-sorted-list.js
@@ -15,6 +15,14 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    
+    let cur = head
+    while(cur && cur.next) {
+      if (cur.val === cur.next.val) {
+        cur.next = cur.next.next
+      } else {
+        cur = cur.next
+      }
+    }
+    return head
 };
 
