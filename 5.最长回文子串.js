9a20c759adbb54be5f032babd5398ff9ceec0f25
Topppy <taoyiming2010@126.com>
2022-10-11 21:26:51 +0800

    5

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -10,6 +10,27 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
+  
+  function palindrome(s,l,r) {
+    while(l>=0 && r < s.length && s[l] === s[r]) {
+      l--;
+      r++;
+    }
+    // 由于slice是左闭右开，所以左侧有效位置应为l+1
+    // while终止的时候l是在有效位的前一位，r在有效位置的后一位
+    return s.slice(l+1, r);
+  }
+  let res = "";
+
+  for(let i=0; i<s.length; i++) {
+    const s1 = palindrome(s, i,i)
+    const s2 = palindrome(s, i, i + 1);
+    console.log({res, s1, s2})
+    res = s1.length > res.length? s1 : res
+    res = s2.length > res.length? s2 : res
+
+  }
+  return res
 
 };
 // @lc code=end
